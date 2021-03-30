8888ad4fb42daa75553a71f17d85da4665c30c55
Ilya Igolnikov <IIgolnikov@bioivt.com>
2021-03-30 20:30:33 +0300

    "Everyone Can Get Rich" calculator

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    let amount = Number(document.getElementById("principal").value);
+    let rate = Number(document.getElementById("rate").value);
+    let years = Number(document.getElementById("years").value);
+    let resultDiv = document.getElementById("result");
+
+    let result = years * amount * rate / 100;
+
+    resultDiv.innerHTML = 'If you deposit ' + amount + ',<br/>' + 'at an interest rate of ' + rate + '%.<br/>' +
+        'You will receive an amount of ' + result + ',<br/>' + 'in the year ' + (new Date().getFullYear() + years);
 }
-        
+
+function onRateChange() {
+    let rate = document.getElementById("rate").value;
+    document.getElementById("rateValue").textContent = rate + '%';
+}
